404cb696a032d498261a0dc60b849afdb16e925d
Yugai Denis <yugai_denis@mail.ru>
2022-09-30 20:57:50 +0600

    Создание модели + Регистрации модели в админке + создание и применение миграции

--- webproject/shop/admin.py
+++ webproject/shop/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from shop.models import Product
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'category', 'balance', 'price')
+    list_filter = ('id', 'title', 'description', 'category', 'balance', 'price')
+    search_fields = ('title', 'description', 'category', 'balance', 'price')
+    fields = ('title', 'description', 'category', 'photo', 'balance', 'price')
+
+admin.site.register(Product, ProductAdmin)
--- webproject/shop/models.py
+++ webproject/shop/models.py
@@ -1,3 +1,55 @@
 from django.db import models
+from django.db.models import TextChoices
 
-# Create your models here.
+
+class CategoryChoices(TextChoices):
+    OTHER = 'other', 'Разное'
+    NOUTBOOK = 'noutbook', 'Ноутбуки'
+    SMARTPHONE = 'smartphone', 'Смартфоны'
+    TV = 'tv', 'Телевизоры'
+    MONITOR = 'monitor', 'Мониторы'
+
+
+class Product(models.Model):
+    title = models.CharField(
+        verbose_name = 'Наименование товара',
+        max_length = 100,
+        null = False,
+        blank = False
+    )
+    description = models.TextField(
+        verbose_name = 'Оисание товара',
+        max_length = 2000,
+        null = False,
+        blank = False,
+        default = ''
+    )
+    photo = models.CharField(
+        verbose_name = 'Фото товара',
+        max_length = 100,
+        null = False,
+        blank = False,
+        default = ''
+    )
+    category = models.CharField(
+        verbose_name = 'Категория',
+        max_length = 20,
+        choices = CategoryChoices.choices,
+        null = False,
+        blank = False,
+        default = 'other'
+    )
+    balance = models.IntegerField(
+        verbose_name = 'Остаток',
+        null = False,
+        blank = False,
+        default = 0
+    )
+    price = models.DecimalField(
+        verbose_name = 'Стоимость',
+        decimal_places = 2,
+        max_digits = 7,
+        null = False,
+        blank = False,
+        default = 0
+    )

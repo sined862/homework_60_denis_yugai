ae50f2e5958cbeb53b29df1e761ab263885678bd
Yugai Denis <yugai_denis@mail.ru>
2022-10-04 13:17:45 +0600

    Создание шаблона, view и пути - Обновление записи

--- webproject/shop/urls.py
+++ webproject/shop/urls.py
@@ -1,10 +1,11 @@
 ﻿from django.urls import path
 from shop.views.base import index_view
-from shop.views.products import product_view, product_add_view
+from shop.views.products import product_view, product_add_view, product_update_view
 
 
 urlpatterns = [
     path('', index_view, name='index'),
     path('product/<int:pk>', product_view, name='product'),
-    path('product_create/', product_add_view, name='product_add')
+    path('product_create/', product_add_view, name='product_add'),
+    path('product_update/<int:pk>', product_update_view, name='product_update')
 ]
--- webproject/shop/views/products.py
+++ webproject/shop/views/products.py
@@ -1,4 +1,5 @@
-﻿from django.shortcuts import redirect, render, get_object_or_404
+﻿from math import prod
+from django.shortcuts import redirect, render, get_object_or_404
 from shop.models import Product, CategoryChoices
 from shop.forms import ProductForm
 
@@ -19,7 +20,37 @@ def product_add_view(request):
         return render(request, 'product_create.html', context)
     form = ProductForm(request.POST)
     if not form.is_valid():
-        context = {'from': form}
+        context = {'form': form}
         return render(request, 'product_create.html', context)
     product = Product.objects.create(**form.cleaned_data)
     return redirect('product', pk=product.pk)
+
+
+def product_update_view(request, pk):
+    product = get_object_or_404(Product, pk=pk)
+    if request.method == 'GET':
+        form = ProductForm(initial={
+            'title': product.title,
+            'description': product.description,
+            'category': product.category,
+            'photo': product.photo,
+            'balance': product.balance,
+            'price': product.price
+        })
+        return render(request, 'product_update.html', context={'form': form, 'product': product})
+    elif request.method == 'POST':
+        form = ProductForm(data=request.POST)
+        if form.is_valid():
+            product.title = form.cleaned_data['title']
+            product.description = form.cleaned_data['description']
+            product.category = form.cleaned_data['category']
+            product.photo = form.cleaned_data['photo']
+            product.balance = form.cleaned_data['balance']
+            product.price = form.cleaned_data['price']
+            product.save()
+            return redirect('product', pk=product.pk)
+        else:
+            return render(request, 'product_update.html', context={'form': form, 'product': product})
+
+
+            
